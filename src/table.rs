14950e0eba2fd1ed95aa1c0c81a680c46a81e44d
qgaye <qgaye0601@gmail.com>
2023-02-20 23:18:44 +0800

    impl read_cache_block

--- src/table.rs
+++ src/table.rs
@@ -7,7 +7,7 @@ mod iterator;
 use std::path::Path;
 use std::sync::Arc;
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 pub use builder::SsTableBuilder;
 use bytes::{Buf, BufMut, Bytes};
 pub use iterator::SsTableIterator;
@@ -127,7 +127,14 @@ impl SsTable {
 
     /// Read a block from disk, with block cache. (Day 4)
     pub fn read_block_cached(&self, block_idx: usize) -> Result<Arc<Block>> {
-        self.read_block(block_idx)
+        if let Some(block_cache) = &self.block_cache {
+            let block = block_cache
+                .try_get_with((self.id, block_idx), || self.read_block(block_idx))
+                .map_err(|e| anyhow!("{}", e))?;
+            Ok(block)
+        } else {
+            self.read_block(block_idx)
+        }
     }
 
     /// Find the block that may contain `key`.
